ac77593e41fae766fcdb885b2483ac85f9ff3763
roberthdanielt <roberthdanielt@gmail.com>
2024-09-04 15:47:30 -0500

    add NotFoundPageComp, update router index

---
+++ src/components/NotFoundPageComp.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="not-found-comp">
+        <h1 class="title">404</h1>
+        <h1 class="title">Page not found</h1>
+        <p class="subtitle">Sorry, the page you are looking for does not exist.</p>
+        <p class="subtitle">It must have been lost between the backend and the frontend.</p>
+        <router-link to="/" class="description">
+            Volver al inicio
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFoundPage'
+}
+</script>
+
+<style scoped>
+.not-found-comp {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 2rem;
+    font-family: Arial, sans-serif;
+    margin-top: 60px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import MainView from '@/views/MainView.vue'
 import AboutView from '@/views/AboutView.vue'
+import NotFoundPageComp from '@/components/NotFoundPageComp.vue'
 
 const routes = [
   {
@@ -12,7 +13,8 @@ const routes = [
     path: '/about',
     name: 'About',
     component: AboutView
-  }
+  },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundPageComp }
 
 ]
 
