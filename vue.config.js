4866c6ce2f53b49c0116be8053ddd080b42aaa96
Roberth Troya <roberthdanielt@gmail.com>
2025-10-28 17:50:05 -0500

    remove puclickPath into vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' 
-  ? 'https://portafolio.rdtsolutions.site/' 
+  ? '/' 
   : '/',
   outputDir: 'dist',
   assetsDir: 'static',
